fc9a38be3aba49f513b3980d798d77934e86a10d
Yeongchan Kim <hades1@kakao.com>
2024-12-01 12:01:24 +0900

    style: 출력 형식 변경

--- main.py
+++ main.py
@@ -2,14 +2,17 @@ from FullNode import FullNode
 
 fullNode = FullNode()
 
-while True:
-    query = input()
+print("snapshot transactions: 현재 처리된 트랜잭션 정보\n"
+      + "snapshot utxoset: 현재 UTXOset 정보\n")
 
+while True:
+    query = input("% ")
     if query == "snapshot transactions":
         info = fullNode.processedTransactionInfo
         for i in range(len(info)):
             txid, result = info[i]
             print(f"transction{i}: {txid}, validity check: {result}")
+        print()
 
     elif query == "snapshot utxoset":
         utxoSet = fullNode.UTXOSet
@@ -17,6 +20,8 @@ while True:
         for key, utxo in utxoSet.items():
             print(f"utxo{i}: {utxo["txid"]}, {utxo["vout"]}, {utxo["value"]}, {utxo["scriptPubKey"]}")
             i += 1
+        print()
 
     else:
         print("Invalid query entered")
+        break
